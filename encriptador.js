183b1f30797af52559536f01a037e328fa51e458
Jom <jorge.oyola.moreno@gmail.com>
2022-04-27 23:45:40 -0300

    segundo intento de crear una github page

---
+++ encriptador.js
@@ -0,0 +1,9 @@
+var texto = getElementById('textarea').value;
+
+function encriptar(){
+    var texto2 = texto.replace(/e/ig,enter);
+    var textoCifrado = document.getElementById("resultado");
+    
+}
+
+
